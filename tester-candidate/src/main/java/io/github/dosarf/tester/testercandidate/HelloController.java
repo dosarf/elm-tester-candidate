637c342d72bfbc5d46c49d3b723d2144e5deaa32
Ferenc Dosa-Racz <dosarf@gmail.com>
2020-01-25 23:57:36 +0200

    Move sample RestController to path /hello

--- tester-candidate/src/main/java/io/github/dosarf/tester/testercandidate/HelloController.java
+++ tester-candidate/src/main/java/io/github/dosarf/tester/testercandidate/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot!";
     }
--- tester-candidate/src/test/java/io/github/dosarf/tester/testercandidate/TesterCandidateApplicationTests.java
+++ tester-candidate/src/test/java/io/github/dosarf/tester/testercandidate/TesterCandidateApplicationTests.java
@@ -21,7 +21,7 @@ class TesterCandidateApplicationTests {
 
 	@Test
 	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
